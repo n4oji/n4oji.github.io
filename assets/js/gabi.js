ffbad9b0856d4dc619274fb11c540a3d74d3c551
Naoji <naoji.okamoto@gmail.com>
2024-06-14 14:42:11 -0300

    update

--- assets/js/gabi.js
+++ assets/js/gabi.js
@@ -51,3 +51,22 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tab;
+	
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+	
+  tab = document.getElementsByClassName("tab");
+  for (i = 0; i < tab.length; i++) {
+    tab[i].className = tab[i].className.replace(" active", "");
+  }
+	
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+	
+}
